e8eb13105fda62114cb68f652c1940bca7d4d965
Haydn <trolllemon@protonmail.com>
2024-05-06 16:22:35 -0700

    obstacle carving navmesh, enemies avoid light, until it goes out...
    Signed-off-by: Haydn <trolllemon@protonmail.com>

--- Assets/Scripts/AimController.cs
+++ Assets/Scripts/AimController.cs
@@ -17,6 +17,8 @@ public class AimController : MonoBehaviour
 
     public GameObject torch;
 
+    public GameObject wall;
+
     public GameObject range;
     void Start()
     {
@@ -31,7 +33,12 @@ public class AimController : MonoBehaviour
 
     private void Update()
     {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
 
+            Vector3 place = range.transform.position;
+            Instantiate(wall,place,Quaternion.identity);
+        }
         if(Input.GetKeyDown(KeyCode.Space))
         {
 
