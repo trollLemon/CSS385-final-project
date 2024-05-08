1a80fcb944b71ab3251efb0eb9cef87f2a794186
haydn <trolllemon@protonmail.com>
2024-05-07 17:03:12 -0700

    gold and counter, enemy prefab

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,6 +11,7 @@ public class Enemy : MonoBehaviour
     public GameObject[] targets;
     public GameObject exit;
     private int choice;
+    public int goldHeld=0;
     
     NavMeshAgent agent;
     // Start is called before the first frame update
@@ -19,9 +20,13 @@ public class Enemy : MonoBehaviour
      targets = new GameObject[2];
      
      targets[0] = GameObject.Find("Player");
-     targets[1] = GameObject.Find("Gold");
+     targets[1] = GameObject.Find("GoldPile");
+     exit = GameObject.Find("Exit");
      choice = Random.Range(0, 2);
      target=targets[choice].transform;
+
+     Gold gold = targets[1].GetComponent<Gold>();
+     if(gold.gold == 0) target=targets[0].transform;
      agent=GetComponent<NavMeshAgent>();
      agent.updateRotation=false;
      agent.updateUpAxis=false;   
@@ -42,13 +47,17 @@ public class Enemy : MonoBehaviour
 
     void PerformNextAction(){
 
-        Debug.Log("Choice");
         
-        if(transform.position.x==exit.transform.position.x && transform.position.y==exit.transform.position.y){
+
+        // if we are at an exit, we should destory the object, this will only happen if the enemy was going to the exit
+        // so we do not need to worry about if this statement executes when the enemy attacks the player. 
+
+        if(Vector3.Distance(transform.position, exit.transform.position)<2f){
             Destroy(gameObject);
             return;
         }
-        
+       
+    
         if(choice==0){
 
         } 
@@ -56,7 +65,15 @@ public class Enemy : MonoBehaviour
         if( choice ==1){
             Debug.Log("go to exit");
             //pick up gold
-
+            Gold gold = targets[choice].GetComponent<Gold>();
+            
+            // if the enemy reaches the gold pile, but there is no gold, rage against the player
+            if(gold.gold==0){
+                target=targets[0].transform;
+                return;
+            }
+            gold.Take(1);
+            goldHeld++;
             // set the target so the agent can run to exit (off screen)
             target=exit.transform;
         }
