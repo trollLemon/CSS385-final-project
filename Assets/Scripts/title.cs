a055f446309b9240e32fbc853d6baba2986050af
Taschen <taschen@uw.edu>
2024-05-22 15:35:36 -0700

    title fadein

--- Assets/Scripts/title.cs
+++ Assets/Scripts/title.cs
@@ -9,17 +9,36 @@ public class title : MonoBehaviour
     public Image logo;
     public Button startButton;
 
+    // Chris Code
+    // private SpriteRenderer logoSpriteRenderer;
+    Color color;
+    private float startTime;
+
     // Start is called before the first frame update
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
         // StartCoroutine(FadeTitle());
+
+
+        // Chris Code
+        // logoSpriteRenderer = logo.GetComponent<SpriteRenderer>();
+        color = logo.color;
+        color.a = 0.0f;
+        logo.color = color;
+        startTime = Time.time;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (color.a != 1.0f)
+        {
+            color.a = Time.time - startTime;
+            logo.color = color;
+        }
+        
     }
 
     IEnumerator FadeTitle() {
