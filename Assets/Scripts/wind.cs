32b08e64d5b5144c87c76536101ddf737f8a392e
Taschen <taschen@uw.edu>
2024-05-27 20:20:47 -0700

    now with 2 winds

--- Assets/Scripts/wind.cs
+++ Assets/Scripts/wind.cs
@@ -34,16 +34,21 @@ public class wind : MonoBehaviour
         Bounds playarea = boundaryCol.bounds;
         Vector2 temp = new Vector2(Random.Range(playarea.min.x, playarea.max.x), Random.Range(playarea.min.y, playarea.max.y));
         GameObject thisWind = Instantiate(windPrefab, temp, transform.rotation);
+        temp = new Vector2(Random.Range(playarea.min.x, playarea.max.x), Random.Range(playarea.min.y, playarea.max.y));
+        GameObject thisWind2 = Instantiate(windPrefab, temp, transform.rotation);
         
         // slight variation in size
         float randomScale = Random.Range(0.5f, 1.5f);
         thisWind.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
+        randomScale = Random.Range(0.5f, 1.5f);
+        thisWind2.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
         
         // get anim clip length
         AnimatorClipInfo[] windclip = thisWind.GetComponent<Animator>().GetCurrentAnimatorClipInfo(0);
         float animTime = windclip[0].clip.length;
         // auto destroy object on animation completion
         Destroy(thisWind, animTime);
+        Destroy(thisWind2, animTime);
 
     }
 }
